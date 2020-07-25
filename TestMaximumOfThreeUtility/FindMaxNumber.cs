513d52323e318604df7c14278354b70f665d7129
puneet <puneet15159@iiitd.ac.in>
2020-07-25 14:34:49 +0530

    [puneet] Added Method to print max

--- TestMaximumOfThreeUtility/FindMaxNumber.cs
+++ TestMaximumOfThreeUtility/FindMaxNumber.cs
@@ -46,37 +46,44 @@ namespace FindMaximumUtility
             {
                 this.maxElement = value;
             }
+            printmax(this.maxElement);
 
             return this.maxElement;
+
             
         }
 
-       /* public float findMax(float numberOne, float numberTwo, float numberThree)
+        public void printmax(E maxElement)
         {
-            if (numberOne > numberTwo && numberOne > numberThree)
-            {
-                return numberOne;
-            }
-            else if (numberTwo > numberOne && numberTwo > numberThree)
-            {
-                return numberTwo;
-            }
-            else
-                return numberThree;
+            Console.WriteLine(maxElement);
         }
 
-        public string findMax (string numberOne, string numberTwo, string numberThree)
-        {
-            if (String.Compare(numberOne, numberTwo) > 0 && String.Compare(numberOne, numberThree) > 0)
-            {
-                return numberOne;
-            }
-            else if (String.Compare(numberTwo, numberOne) > 0 && String.Compare(numberTwo, numberThree) > 0)
-            {
-                return numberTwo;
-            }
-            else
-                return numberThree;
-        }*/
+        /* public float findMax(float numberOne, float numberTwo, float numberThree)
+         {
+             if (numberOne > numberTwo && numberOne > numberThree)
+             {
+                 return numberOne;
+             }
+             else if (numberTwo > numberOne && numberTwo > numberThree)
+             {
+                 return numberTwo;
+             }
+             else
+                 return numberThree;
+         }
+
+         public string findMax (string numberOne, string numberTwo, string numberThree)
+         {
+             if (String.Compare(numberOne, numberTwo) > 0 && String.Compare(numberOne, numberThree) > 0)
+             {
+                 return numberOne;
+             }
+             else if (String.Compare(numberTwo, numberOne) > 0 && String.Compare(numberTwo, numberThree) > 0)
+             {
+                 return numberTwo;
+             }
+             else
+                 return numberThree;
+         }*/
     }
 }
